3f2b2a9eb380cb52607a36d1a2c1870869f3f3af
kwn1996kr <kwn1996kr@naver.com>
2022-05-12 23:45:54 +0900

    fix : 채팅 프로필 수정

--- app.py
+++ app.py
@@ -561,7 +561,7 @@ def my_room_event(message):
     payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
     username = payload['id']
     user_info = db.users.find_one({"username": username}, {"_id": False})
-    if(user_info['profile_pis']=='') :
+    if(user_info['profile_pic']=='') :
         emit('my_response',
              {'data': message['data'], 'count': session['receive_count'], 'type': 1, 'name': user_info['nickname'],
               'image': user_info['profile_pic_real']},
